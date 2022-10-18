2772fb9f436024c5aa613e2805a993c0f95a84ab
jose izaias alcantara lima <joseizaias@gmail.com>
2022-10-18 17:55:03 -0300

    move business login to a domain entity

--- src/data/services/facebook-authentication.ts
+++ src/data/services/facebook-authentication.ts
@@ -2,24 +2,21 @@ import { LoadFacebookUserApi } from '@/data/contracts/apis'
 import { SaveFacebookAccountRepository, LoadUserAccountRepository } from '@/data/contracts/repos'
 import { AuthenticationError } from '@/domain/errors'
 import { FacebookAuthentication } from '@/domain/features'
+import { FacebookAccount } from '@/domain/models'
 
 export class FacebookAuthenticationService {
   constructor (
     private readonly facebookApi: LoadFacebookUserApi,
-    private readonly UserAccountRepo: LoadUserAccountRepository & SaveFacebookAccountRepository
+    private readonly userAccountRepo: LoadUserAccountRepository & SaveFacebookAccountRepository
   ) {}
 
   async perform (params: FacebookAuthentication.Params): Promise<AuthenticationError> {
     const fbData = await this.facebookApi.loadUser(params)
 
     if (fbData !== null && fbData !== undefined) {
-      const accountData = await this.UserAccountRepo.load({ email: fbData.email })
-      await this.UserAccountRepo.saveWithFacebook({
-        id: accountData?.id,
-        name: accountData?.name ?? fbData.name,
-        email: fbData.email,
-        facebookId: fbData.facebookId
-      })
+      const accountData = await this.userAccountRepo.load({ email: fbData.email })
+      const fbAccount = new FacebookAccount(fbData, accountData)
+      await this.userAccountRepo.saveWithFacebook(fbAccount)
     }
 
     return new AuthenticationError()
